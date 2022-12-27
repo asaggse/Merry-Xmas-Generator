83c7778d622b6a87ec0251607c38da3538f8f785
Alessandro Saggese <asaggese@gmail.com>
2022-12-27 18:27:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const changeImageButton = document.querySelector('#change-image');
+const previewImage = document.querySelector('preview-text');
 
 const assetsCount = 4;
 let currentImageIndex = 1;
