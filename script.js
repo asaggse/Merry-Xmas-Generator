08eeaa9125779e624ef3f13f22b2974c4237c636
Alessandro Saggese <asaggese@gmail.com>
2022-12-27 18:55:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const changeImageButton = document.querySelector('#change-image');
+const changeTextButton = document.querySelector('#change-text');
 const previewImage = document.querySelector('#preview-image');
 const previewText = document.querySelector('#preview-text');
 
 const assetsCount = 4;
 let currentImageIndex = 1;
+let currentTextIndex = 1;
 
 function changeImage() {
    currentImageIndex+=1;
@@ -14,4 +16,14 @@ function changeImage() {
    previewImage.src = `./images/img_${currentImageIndex}.png`;
 }
 
+function changeText() {
+   currentTextIndex+=1;
+   if(currentTextIndex > assetsCount) {
+      currentTextIndex = 1;
+   }
+   console.log('cambio testo', currentTextIndex);
+   previewText.src = `./images/txt_${currentTextIndex}.png`;
+}
+
 changeImageButton.addEventListener('click', changeImage);
+changeTextButton.addEventListener('click', changeText);
