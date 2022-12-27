490f9f60fc6ca679ce9a189fd1b1d760478163f2
Alessandro Saggese <asaggese@gmail.com>
2022-12-27 19:12:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,40 @@ const assetsCount = 4;
 let currentImageIndex = 1;
 let currentTextIndex = 1;
 
-function changeImage() {
-   currentImageIndex+=1;
-   if(currentImageIndex > assetsCount) {
-      currentImageIndex = 1;
+// function changeImage() {
+//    currentImageIndex+=1;
 //    if(currentImageIndex > assetsCount) {
+//       currentImageIndex = 1;
+//    }
+//    console.log('cambio immagine', currentImageIndex);
+//    previewImage.src = `./images/img_${currentImageIndex}.png`;
+// }
+
+// function changeText() {
+//    currentTextIndex+=1;
+//    if(currentTextIndex > assetsCount) {
+//       currentTextIndex = 1;
+//    }
+//    console.log('cambio testo', currentTextIndex);
+//    previewText.src = `./images/txt_${currentTextIndex}.png`;
+// }
+
+// changeImageButton.addEventListener('click', changeImage);
+// changeTextButton.addEventListener('click', changeText);
+
+function changeAsset(assetType) {
+   let currentIndex;
+   let targetElement;
+   let srcPrefix;
+
+   if (assetType === 'image') {
+      currentIndex = currentImageIndex;
+      targetElement = previewImage;
+      srcPrefix = 'img';
+   } else if (assetType === 'text') {
+      currentIndex = currentTextIndex;
+      targetElement = previewText;
+      srcPrefix = 'txt';
    }
 
    currentIndex += 1;
